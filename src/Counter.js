eb5ac50b5760d2834fa03b7ad42846c9d4a6390e
Tanzeel Ansari <121967095+gi-t-ansari@users.noreply.github.com>
2023-11-21 09:15:12 +0530

    Add files via upload

---
+++ src/Counter.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      counter: 0
+    });
+  };
+
+  render() {
+    return (
+      <div id="container">
+        <h1>COUNTER APP</h1>
+        <h2 id="counter">{this.state.counter}</h2>
+        <button onClick={this.increment} className="button increment">Increment</button>
+        {this.state.counter === 0 ? <button disabled className="disabled">Decrement</button> :<button onClick={this.decrement} className="button decrement">Decrement</button>}
+        {this.state.counter === 0 ? <button disabled className="disabled">Reset</button> :<button onClick={this.reset} className="button reset">Reset</button>}
+      </div>
+    );
+  }
+}
+
+export default Counter;
